51d2de5c69aa642605b9a80b26299d71c99f7e32
NBesingi <mosinaagnesb@gmail.com>
2023-10-13 17:34:55 +0100

    Fri 13/10/23- learnt  react components and props

--- myfirstreact/src/Components.js
+++ myfirstreact/src/Components.js
@@ -1,1 +1,18 @@
 import './App.css'
+function App() {
+    return(<div classname="App">
+       <Job company="Afrovision" position="UI/UX designer" salary={50000} /> 
+       <Job company="Speednet" position="developer" salary={60000} /> 
+       <Job company="Summit tech" position="CEO" salary={100000} /> 
+
+    </div>); 
+}
+const Job=(props) =>{
+    <><h1>{props.company}</h1>
+    <h1>{props.position}</h1>
+    <h1>{props.salary}</h1></>
+
+
+}
+
+export default App;
